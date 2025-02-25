249127eb06323e9f7e61ac0af9539d51f9f27eb3
jungmin <1qsonjungmin@gmail.com>
2025-02-25 17:21:38 +0900

    [level 2] Title: 카펫, Time: 171.17 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/2/42842. 카펫/카펫.py
+++ Python3/프로그래머스/2/42842. 카펫/카펫.py
@@ -1,8 +1,8 @@
 def solution(brown, yellow):
-    total = brown + yellow  # 전체 타일 개수
-    
-    for h in range(1, int(total**0.5) + 1):  # 가능한 높이(h)를 탐색
-        if total % h == 0:  # h가 total의 약수라면
-            w = total // h  # 너비 계산
-            if (w - 2) * (h - 2) == yellow:  # 노란색 타일 개수 검증
-                return [w, h]  # 가로, 세로 반환
+    answer = []
+    for w in range(1, brown + yellow + 1):
+        if (brown + yellow) % w == 0:
+            h = (brown + yellow) // w
+            if (w - 2) * (h - 2) == yellow:
+                answer = [w, h]
+    return answer
