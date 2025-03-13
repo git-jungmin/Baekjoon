1b242c9d79c3f8b84218a6808df010e60876fc87
jungmin <1qsonjungmin@gmail.com>
2025-03-13 10:17:04 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,19 +1,11 @@
-import math
-
 def solution(progresses, speeds):
     answer = []
-    days = [math.ceil((100 - p) / s) for p, s in zip(progresses, speeds)]
-    
-    deploy_day = days[0]
-    count = 0
-
-    for day in days:
-        if day > deploy_day:
-            answer.append(count)
-            deploy_day = day
-            count = 1
+    days = list(map(lambda x, y: (100 - x) // y if (100 - x) // y == (100 - x) / y else (100 - x) // y + 1 ,progresses, speeds))
+    for i in days:
+        if len(answer) == 0:
+            answer.append(1)
+        elif days[sum(answer) - 1] >= i:
+            answer[len(answer)] += 1
         else:
-            count += 1
-
-    answer.append(count)  # Append the last group
+            answer.append(1)
     return answer
