d9fb2cd8f1d23124f43a6e4ee34dab54ee83eda1
jungmin <1qsonjungmin@gmail.com>
2025-09-15 23:41:31 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,21 @@
+SELECT 
+    E.EMP_NO, 
+    E.EMP_NAME, 
+    G.GRADE, 
+    CASE 
+        WHEN G.GRADE = 'S' THEN E.SAL * 0.2 
+        WHEN G.GRADE = 'A' THEN E.SAL * 0.15 
+        WHEN G.GRADE = 'B' THEN E.SAL * 0.1 
+        ELSE 0 
+    END AS BONUS
+FROM HR_EMPLOYEES E
+JOIN (SELECT EMP_NO, 
+      CASE
+        WHEN AVG(SCORE) >= 96 THEN 'S'
+        WHEN AVG(SCORE) >= 90 THEN 'A'
+        WHEN AVG(SCORE) >= 80 THEN 'B'
+        ELSE 'C'
+      END AS GRADE
+      FROM HR_GRADE
+      GROUP BY EMP_NO) G ON E.EMP_NO = G.EMP_NO
+ORDER BY EMP_NO
