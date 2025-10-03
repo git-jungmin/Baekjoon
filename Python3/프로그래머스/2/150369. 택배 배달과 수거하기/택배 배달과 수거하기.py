031621f8f80c5d800dbcb01dc037477fe7c3502e
jungmin <1qsonjungmin@gmail.com>
2025-10-03 18:13:10 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 3148.69 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -0,0 +1,21 @@
+def solution(cap, n, deliveries, pickups):
+    def update(current,dist,idx):
+        for i in range(idx-1,-1,-1):
+            if current[i] > 0:
+                current[i] -= 1
+                if i > dist - 1:
+                    dist = i + 1
+                return current, dist, i + 1
+        return current, dist, idx
+    
+    answer = 0
+    idx_d,idx_p = n,n
+    while True:
+        dist = 0
+        for c in range(cap):
+            deliveries, dist, idx_d = update(deliveries,dist,idx_d)
+            pickups, dist, idx_p = update(pickups,dist,idx_p)
+        if dist == 0:
+            break
+        answer += dist * 2
+    return answer
