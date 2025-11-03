0ab8919d266a690717cd2c321eaf8fc9c7c52496
jungmin <1qsonjungmin@gmail.com>
2025-11-03 11:27:55 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.51 ms, Memory: 87 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        Arrays.sort(sides);
+        int a = sides[0];
+        int b = sides[1];
+        int c = sides[2];
+        if(a + b > c) return 1;
+        return 2;
+    }
+}
