eb88490207839fac6b9d93b8f5468d1b9639b214
jungmin <1qsonjungmin@gmail.com>
2025-11-14 16:11:12 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.72 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        String ab = String.valueOf(a) + String.valueOf(b);
+        String ba = String.valueOf(b) + String.valueOf(a);
+        return Math.max(Integer.parseInt(ab),Integer.parseInt(ba));
+    }
+}
