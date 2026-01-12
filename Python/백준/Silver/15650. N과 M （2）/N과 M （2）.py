541f7da6dd22c868ab4e126684845c393354ac4a
jungmin <1qsonjungmin@gmail.com>
2026-01-12 17:51:48 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N,M = map(int,input().split(" "))
+
+arr = []
+
+def dfs(start):
+    if (len(arr) == M):
+        print(*arr)
+        return
+    
+    for i in range(start, N+1):
+        arr.append(i)
+        dfs(i+1)
+        arr.pop()
+
+dfs(1)
