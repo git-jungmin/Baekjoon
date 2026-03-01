696582423b89c8549da47f32801d994997707815
jungmin <1qsonjungmin@gmail.com>
2026-03-01 12:55:41 +0900

    [Silver III] Title: 1로 만들기, Time: 764 ms, Memory: 71968 KB -BaekjoonHub

---
+++ Python/백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+INF = 1e9
+
+dp = [INF] * (N + 1)
+dp[N] = 0
+
+for i in range(N,0,-1):
+    if i % 3 == 0:
+        dp[i//3] = min(dp[i//3],dp[i]+1)
+    if i % 2 == 0:
+        dp[i//2] = min(dp[i//2],dp[i]+1)
+    if i > 1:
+        dp[i - 1] = min(dp[i-1], dp[i]+1)
+
+print(dp[1])
