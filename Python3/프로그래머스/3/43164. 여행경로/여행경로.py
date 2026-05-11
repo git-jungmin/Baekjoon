8f713a2aea092174e4fd0a11f9db29caed08ffe0
jungmin <1qsonjungmin@gmail.com>
2026-05-11 16:39:43 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 9.18 MB -BaekjoonHub

--- Python3/프로그래머스/3/43164. 여행경로/여행경로.py
+++ Python3/프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,15 +1,40 @@
-from collections import defaultdict
+import heapq
 def solution(tickets):
-    graph = defaultdict(list)
+    route = []
+    graph = dict()
+    
     for start, end in tickets:
+        if start not in graph:
+            graph[start] = []
+        
         graph[start].append(end)
+    
     for key in graph:
         graph[key].sort()
-    route = []
-    def dfs(current):
-        while graph[current]:
-            next_dst = graph[current].pop(0)
-            dfs(next_dst)
-        route.append(current)
+    
+    print(graph)
+    
+    route = ["ICN"]
+    
+    def dfs(start):
+        if len(tickets) + 1 == len(route):
+            return True
+    
+        if start not in graph:
+            return False
+        
+        for i in range(len(graph[start])):
+            next_dest = graph[start][i]
+            graph[start].pop(i)
+            route.append(next_dest)
+            
+            if dfs(next_dest):
+                return True
+            
+            route.pop()
+            graph[start].insert(i, next_dest)
+
     dfs("ICN")
-    return route[::-1]
+    return route
+        
+        
